78212cab2e7275edda6067b1e39ac871fae21e03
Erik Vester <erik@vester.co>
2018-12-04 17:03:09 +0100

    HTML/CSS setup finished + routes

--- battleship-game/routes/index.js
+++ battleship-game/routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/splash', function(req, res) {
+  res.render('splash.html', { title: 'Express' });
+});
+
+
+/* Pressing Play gives this page */ 
+router.get('/game.html', function(req, res) {
+  res.render('game.html', { title: 'Express' });
 });
 
 module.exports = router;
