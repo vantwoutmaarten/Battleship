230783a77eeb9789ab2ca6f93f63fdfcb9ccf09a
Erik Vester <erik@vester.co>
2018-12-06 17:50:49 +0100

    BANG

--- battleship-game/app.js
+++ battleship-game/app.js
@@ -3,24 +3,33 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var http = require('http'); 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var usersRouter = require('./routes/users');
 
 var app = express();
 
+// setting up server
+var port = 3000; 
+var server = http.createServer(app);
+
+//Static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+//
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +47,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+server.listen(port, () => console.log('server started'));
 module.exports = app;
