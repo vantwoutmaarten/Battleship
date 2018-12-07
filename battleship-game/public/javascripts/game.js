fead68dcdf742492517d28035e6d3776857d82cc
Maarten van 't Wout <m.a.vantwout@student.tudelft.nl>
2018-12-07 11:57:38 +0100

    javascript te lauw

--- battleship-game/public/javascripts/game.js
+++ battleship-game/public/javascripts/game.js
@@ -1,8 +1,4 @@
 'use strict';
-
-var game = new BattleShip.Game( $('#gridleft'), $('#gridright'), $('.game_status'), 10, 10 );
-game.start();
-
 /**
  * Game constructor
  * @param {jQuery obj} player1Container
@@ -43,28 +39,29 @@ BattleShip.Game.prototype = {
      * Init player2 field
      */
     initPlayer2Field: function() {
-
-        var table = this.createTable( this.player2Container )
+        var tableR = this.player2Container
           , self = this;
 
         var onShotMissedHandler = function(pos) {
             $(self.gameStatusContainer).text("Other turn");
-            table.find('tr').eq(pos.y).find('td').eq(pos.x).addClass('fired_cell');
+            var s1 = "#"+ pos.x + "\\|" + pos.y;
+            tableR.find(s1).addClass('miss').removeClass('water');
         }
 
         var onShipDamagedHandler = function(pos) {
-            self.markShipDamaged( table, pos );
+           self.markShipDamaged( tableR, pos );
         }
 
         var onShipDiedHandler = function(shipPositions) {
-            self.markShipDied( table, shipPositions );
-            self.markAroundShipDied( table, shipPositions );
+            self.markShipDied( tableR, shipPositions );
+            self.markAroundShipDied( tableR, shipPositions );
         }
 
         var onPlayer2LostHandler = function() {
             $(self.gameStatusContainer).text('Game over');
             alert( "You win! Repeat that." );
-            location.reload();
+            //what does this does again?
+            // location.reload();
         };
 
         this.player2Field = new BattleShip.Field (
@@ -75,17 +72,36 @@ BattleShip.Game.prototype = {
             , this.fieldHeight
             , this.fieldWidth );
 
-        table.on('click', 'td', function() {
-            var xCoordinate = this.cellIndex;
-            var yCoordinate = this.parentNode.rowIndex;
+        // tableR.on('click', 'td', function() {
+        //     var xCoordinate = this.cellIndex;
+        //     var yCoordinate = this.parentNode.rowIndex;
+        //     self.player2Field.makeShot( new BattleShip.Position( xCoordinate, yCoordinate ) );
+        // });
+
+        tableR.on('click', 'div', function() {
+            var selected = $(this).attr("id");
+            var res = selected.split("|");
+
+           var xCoordinate = parseInt(res[0]);
+           var yCoordinate = parseInt(res[1]);
             self.player2Field.makeShot( new BattleShip.Position( xCoordinate, yCoordinate ) );
         });
 
-        $('#rightgrid').on('click', 'div', function() {
-            var xCoordinate = this.index();
-            var yCoordinate = this.parentNode.rowIndex;
-            self.player2Field.makeShot( new BattleShip.Position( xCoordinate, yCoordinate ) );
-        });
+        // show ships on player2 field
+        for ( var i = 0; i < this.player2Field.ships.length; i++ ) {
+            var shipPositions = this.player2Field.ships[i].getPositions();
+            for ( var j = 0; j < shipPositions.length; j++ ) {
+                var pos = shipPositions[j];
+                var s1 = "#"+ pos.x + "\\|" + pos.y;
+                tableR.find(s1).addClass('liveship').removeClass('water');
+            }
+        }
+
+
+        // $("#gridright").children("div").click(function(){
+        //         //if top one does not work this is a alternative;
+        // });
+
     },
 
     /**
@@ -93,27 +109,29 @@ BattleShip.Game.prototype = {
      */
     initPlayer1Field: function() {
 
-        var table = this.createTable( this.player1Container )
+        var tableL = this.player1Container
           , self = this;
 
         var onShotMissedHandler = function(pos) {
             $(self.gameStatusContainer).text("User turn");
-            table.find('tr').eq(pos.y).find('td').eq(pos.x).addClass('fired_cell');
+            var s1 = "#"+ pos.x + "\\|" + pos.y;
+            tableL.find(s1).addClass('miss').removeClass('water');
+    console.log("hello");
         }
 
         var onShipDamagedHandler = function(pos) {
-            self.markShipDamaged( table, pos );
+            self.markShipDamaged( tableL, pos );
         }
 
         var onShipDiedHandler = function(shipPositions) {
-            self.markShipDied( table, shipPositions );
-            self.markAroundShipDied( table, shipPositions );
+            self.markShipDied( tableL, shipPositions );
+            self.markAroundShipDied( tableL, shipPositions );
         }
 
         var onPlayer1LostHandler = function() {
             $(self.gameStatusContainer).text('Game over');
             alert( "You lose. Repeat that." );
-            location.reload();
+    //         location.reload();
         };
 
         this.player1Field = new BattleShip.Field (
@@ -129,9 +147,20 @@ BattleShip.Game.prototype = {
             var shipPositions = this.player1Field.ships[i].getPositions();
             for ( var j = 0; j < shipPositions.length; j++ ) {
                 var pos = shipPositions[j];
-                table.find('tr').eq(pos.y).find('td').eq(pos.x).addClass('live_ship');
+                var s1 = "#"+ pos.x + "\\|" + pos.y;
+                tableL.find(s1).addClass('liveship').removeClass('water');
             }
         }
+
+        tableL.on('click', 'div', function() {
+            var selected = $(this).attr("id");
+            var res = selected.split("|");
+
+           var xCoordinate = parseInt(res[0]);
+           var yCoordinate = parseInt(res[1]);
+            self.player1Field.makeShot( new BattleShip.Position( xCoordinate, yCoordinate ) );
+        });
+
     },
 
 
@@ -141,7 +170,8 @@ BattleShip.Game.prototype = {
      * @param {BattleShip.Position} pos cell position
      */
     markShipDamaged: function(table, pos) {
-        table.find('tr').eq(pos.y).find('td').eq(pos.x).removeClass('fired_cell').addClass('damaged_ship');
+        var s1 = "#"+ pos.x + "\\|" + pos.y;
+        table.find(s1).removeClass('miss').addClass('hit');
     },
 
     /**
@@ -151,7 +181,9 @@ BattleShip.Game.prototype = {
      */
     markShipDied: function(table, shipPositions) {
         for ( var i = 0; i < shipPositions.length; i++ ) {
-            table.find('tr').eq(shipPositions[i].y).find('td').eq(shipPositions[i].x).removeClass('fired_cell').addClass('died_ship');
+            var s1 = "#"+ shipPositions[i].x + "\\|" + shipPositions[i].y;
+            table.find(s1).removeClass('miss').addClass('killed');
+            // table.find('tr').eq(shipPositions[i].y).find('td').eq(shipPositions[i].x).removeClass('miss').addClass('killed');
         }
     },
 
@@ -165,7 +197,9 @@ BattleShip.Game.prototype = {
             for ( var x = shipPositions[i].x - 1; x <= shipPositions[i].x + 1; x++ ) {
                 for ( var y = shipPositions[i].y - 1; y <= shipPositions[i].y + 1; y++ ) {
                     if ( x >= 0 && y >= 0 ) {
-                        table.find('tr').eq(y).find('td').eq(x).addClass('fired_cell');
+                        var s1 = "#"+ shipPositions[i].x + "\\|" + shipPositions[i].y;
+                        table.find(s1).addClass('liveship').removeClass('water');
+                        // table.find('tr').eq(y).find('td').eq(x).addClass('miss');
                     }
                 }
             }
